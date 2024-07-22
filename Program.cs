48c5001fa409b97d0f01f27a91b1e7f46705701b
Calebe Stodulski <calebestodulski@gmail.com>
2024-07-22 10:39:19 -0300

    rebuild

--- Program.cs
+++ Program.cs
@@ -2,12 +2,12 @@ using TodoApi.Models;
 using TodoApi.Services;
 
 var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
-
 builder.Services.Configure<TodoApiDbSettings>(
     builder.Configuration.GetSection("TodoApiDatabase"));
 
 builder.Services.AddSingleton<TodoService>();
+var app = builder.Build();
+
 
 app.MapGet("/todo", async (TodoService db) => await db.GetAsync());
 
