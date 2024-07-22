0e1ae48c28b4400f16cfa59fa79974b2aeb53874
Calebe Stodulski <calebestodulski@gmail.com>
2024-07-22 11:00:04 -0300

    minimal api crud created

--- Program.cs
+++ Program.cs
@@ -10,6 +10,34 @@ var app = builder.Build();
 
 
 app.MapGet("/todo", async (TodoService db) => await db.GetAsync());
+app.MapGet("/todo/{id}", async (TodoService db, string id) => await db.GetAsync(id));
+
+app.MapPost("/todo", async (TodoService db, Todo todo) =>
+{
+    await db.CreateAsync(todo);
+    return Results.Created($"/todo/{todo.Id}", todo);
+});
+
+app.MapPut("/todo/{id}", async (TodoService db, Todo updatedTodo, string id) =>
+{
+    var todo = await db.GetAsync(id);
+    if(todo is null) return Results.NotFound("don't was found any task with this id");
+    todo.Name = updatedTodo.Name;
+    todo.Description = updatedTodo.Description;
+    todo.Done = updatedTodo.Done;
+    todo.ScheduledTime = updatedTodo.ScheduledTime;
+    await db.UpdateAsync(id, updatedTodo);
+    return Results.NoContent(); 
+});
+
+app.MapDelete("/todo/{id}", async (TodoService db, string id) => 
+{
+    var todo = await db.GetAsync(id);
+    if(todo is null) return Results.NotFound("task not found");
+    await db.DeleteAsync(id);
+    return Results.Ok();
+});
+
 
 app.MapGet("/", () => "Hello World!");
 
